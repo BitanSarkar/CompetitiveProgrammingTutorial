c6013e5601b5fe0e78829020f8d252c93f562ec7
Bitan Sarkar <bitansarkar12345@gmail.com>
2025-09-03 13:06:13 +0530

    Optimized

--- Exercise/Main_1_1_3.java
+++ Exercise/Main_1_1_3.java
@@ -1,6 +1,15 @@
+package Exercise;
+
 public class Main_1_1_3 {
 
     public static void main(String[] args) {
+
+        //6.47213595499958
+        // 2.8284271247461903
+        // 8.82842712474619
+        // 13.48528137423857
+        // 18.14213562373095
+
         Solution sol = new Solution();
         int N = 3;
         int[][] points = new int[][] {
@@ -55,29 +64,28 @@ class Solution {
 
     public double shortestDistanceSum(int N, int[][] points) {
         int n = 2*N;
-        boolean[] visited = new boolean[n];
+        int visited = (1 << n) - 1; 
         return dfs(n, points, visited);
     }
 
-    public double dfs(int n, int[][] points, boolean[] visited) {
+    public double dfs(int n, int[][] points, int visited) {
         if(n < 2) return 0;
         double min = Double.MAX_VALUE;
-        for(int i = 0; i < points.length; i++) {
-            for(int j = i+1; j < points.length; j++) {
-                if(!visited[i] && !visited[j]) {
-                    int x1 = points[i][0];
-                    int y1 = points[i][1];
-                    int x2 = points[j][0];
-                    int y2 = points[j][1];
-                    double dist1 = Math.hypot(x1 - x2, y1 - y2);
-                    visited[i] = true;
-                    visited[j] = true;
-                    double dist2 = dfs(n-2, points, visited);
-                    min = Math.min(dist1 + dist2, min);
-                    visited[i] = false;
-                    visited[j] = false;
-
-                }
+        int i = Integer.numberOfTrailingZeros(visited);
+        for(int j = i+1; j < points.length; j++) {
+            int mask1 = 1 << i;
+            int mask2 = 1 << j;
+            int v1 = visited & mask1;
+            int v2 = visited & mask2;
+            if(v1 != 0 && v2 != 0) {
+                int x1 = points[i][0];
+                int y1 = points[i][1];
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                double dist1 = Math.hypot(x1 - x2, y1 - y2);
+                if(dist1 > min) continue;
+                double dist2 = dfs(n-2, points, visited & (~mask1) & (~mask2));
+                min = Math.min(dist1 + dist2, min);
             }
         }
         return min;
